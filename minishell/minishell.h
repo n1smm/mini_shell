25ab9cdc9b381570b30de127dc40d8580f95385d
n1smm <tjaz.juvan@gmail.com>
2024-05-19 13:29:03 +0200

    changed header; token,trash and type

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -12,16 +12,35 @@ typedef struct s_input {
 	char **tokens;
 }	t_input;
 
-typedef enum cmds{
-	ECHO,
-	CD,
-	PWD,
-	EXPORT,
-	UNSET,
-	ENV,
-	EXIT,
-	NOT_VALID
-} t_cmds;
+typedef struct	s_token 
+{
+	t_type	typ_token;
+	/* void 	*value; */
+	char 	*value;
+	long 	value;
+	t_token *next;
+	t_token *previous;
+} 				t_token;
+
+
+typedef struct	s_trash 
+{
+	void *content;
+	t_trash *next;
+}				t_trash;
+
+}
+typedef enum	s_type{
+	DIGIT,
+	ALPHA,
+	NUMBER,
+	WORD,
+	PIPELINE,
+	REDIRECT,
+	EXPAND,
+	COMMAND,
+	OPTION,
+} 				t_type;
 
 /* lexer */
 void lexer_main(char *input);
