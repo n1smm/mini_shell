0bcdbe13062183aaee78caa427ab49407845dfcb
n1smm <tjaz.juvan@gmail.com>
2024-05-21 14:52:00 +0200

    added make_list for list API and changed a little enums and token declarations in .h; also added important notes file to write notes if needed

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -6,7 +6,7 @@
 /*   By: pgiorgi <pgiorgi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 13:46:03 by thiew             #+#    #+#             */
-/*   Updated: 2024/05/19 16:29:38 by pgiorgi          ###   ########.fr       */
+/*   Updated: 2024/05/21 14:42:03 by thiew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,12 @@ typedef struct s_input
 
 typedef enum s_type
 {
-	DIGIT,
-	ALPHA,
+	WHITESPACE,
 	NUMBER,
 	WORD,
 	PIPELINE,
-	REDIRECT,
+	REDIRECT_IN,
+	REDIRECT_OUT,
 	EXPAND,
 	COMMAND,
 	OPTION,
@@ -42,9 +42,7 @@ typedef enum s_type
 typedef struct s_token
 {
 	t_type	typ_token;
-	// void 	*value;
-	char	*str;
-	long	nbr;
+	char	*content;
 	struct s_token	*next;
 	struct s_token	*prev;
 }			t_token;
@@ -57,14 +55,18 @@ typedef struct s_trash
 
 
 /* lexer */
-void		lexer_main(char *input);
+void			lexer_main(char *input);
 /* safe_functions */
-void		*safe_malloc(size_t size);
+void			*safe_malloc(size_t size);
 /*split_input*/
-void split_input(char *input);
-bool term_character(char c);
-char	*ft_chardup(char character);
-
+void 			split_input(char *input);
+bool 			term_character(char c);
+char			*ft_chardup(char character);
 /*list functions*/
-void	ft_add_token(char *word);
-void	ft_add_token2(char character);
+void			ft_add_token(char *word);
+void			ft_add_token2(char character);
+/* make_list */
+t_token			*init(char *content, t_token **tail, t_token **head);
+bool			double_lstadd_front(char *content, t_token **tail);
+bool			double_lstadd_back(char *content, t_token **head);
+void			print_list(t_token *tail);
