48c3a15c278ae4b662624e7d481b1780f206ba83
Petra Giorgi <pgiorgi@c1r3p1.42firenze.it>
2024-09-25 16:25:13 +0200

    Added exit error code to cd

--- minishell/built_ins/cd.c
+++ minishell/built_ins/cd.c
@@ -6,7 +6,7 @@
 /*   By: pgiorgi <pgiorgi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 17:07:41 by pgiorgi           #+#    #+#             */
-/*   Updated: 2024/06/24 11:08:49 by pgiorgi          ###   ########.fr       */
+/*   Updated: 2024/09/25 16:24:24 by pgiorgi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,13 @@ bool	ft_cd(const char *path, char **input)
 		i++;
 	if (i > 2)
 	{
+		g_error_code = 1;
 		printf("-bash: cd: too many arguments");
 		return (true);
 	}
 	if (chdir(path) == -1)
 	{
+		g_error_code = 1;
 		printf("-bash: cd: %s: No such file or directory", path);
 	}
 	else
