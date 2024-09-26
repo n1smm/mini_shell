ce06f1b2042315f87d821c5309152c8e17b37c05
n1smm <tjaz.juvan@gmail.com>
2024-09-26 16:51:29 +0200

    utils_main norminetted

--- minishell/utils_main.c
+++ minishell/utils_main.c
@@ -6,11 +6,11 @@
 /*   By: thiew <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 15:03:55 by thiew             #+#    #+#             */
-/*   Updated: 2024/09/26 15:53:08 by thiew            ###   ########.fr       */
+/*   Updated: 2024/09/26 16:47:42 by thiew            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "executor/pipex.h"
+#include "minishell.h"
 
 size_t	count_env_vars(char **env)
 {
@@ -20,6 +20,35 @@ size_t	count_env_vars(char **env)
 	while (env[env_num])
 		env_num++;
 	return (env_num);
+}
+
+static void	shell_iteration(t_shell **data, char **env, size_t *i)
+{
+	while (*i < (*data)->num_env_var)
+	{
+		(*data)->env[*i] = ft_strdup(env[*i]);
+		if (!(*data)->env[*i])
+		{
+			free_garbage(&((*data)->garbage));
+			*data = NULL;
+			return ;
+		}
+		add_to_garbage(&((*data)->garbage), (*data)->env[*i]);
+		(*i)++;
+	}
+	(*data)->env[(*data)->num_env_var] = NULL;
+	*i = -1;
+	while (++(*i) < (*data)->num_env_var)
+	{
+		(*data)->exp[*i] = ft_strdup_exp(env[*i]);
+		if (!(*data)->exp[*i])
+		{
+			free_garbage(&((*data)->garbage));
+			*data = NULL;
+			return ;
+		}
+		add_to_garbage(&((*data)->garbage), (*data)->exp[*i]);
+	}
 }
 
 void	ft_init_shell(t_shell **data, char **env)
@@ -35,39 +64,28 @@ void	ft_init_shell(t_shell **data, char **env)
 	add_to_garbage(&((*data)->garbage), (*data)->exp);
 	add_to_garbage(&((*data)->garbage), *data);
 	i = 0;
-	while (i < (*data)->num_env_var)
-	{
-		(*data)->env[i] = ft_strdup(env[i]);
-		if (!(*data)->env[i])
-		{
-			free_garbage(&((*data)->garbage));
-			*data = NULL;
-			return ;
-		}
-		add_to_garbage(&((*data)->garbage), (*data)->env[i]);
-		i++;
-	}
-	(*data)->env[(*data)->num_env_var] = NULL;
-	i = 0;
-	while (i < (*data)->num_env_var)
-	{
-		(*data)->exp[i] = ft_strdup_exp(env[i]);
-		if (!(*data)->exp[i])
-		{
-			free_garbage(&((*data)->garbage));
-			*data = NULL;
-			return ;
-		}
-		add_to_garbage(&((*data)->garbage), (*data)->exp[i]);
-		i++;
-	}
+	shell_iteration(data, env, &i);
 	(*data)->exp[(*data)->num_env_var] = NULL;
 	(*data)->next = NULL;
 }
 
+static void	last_iteration(const char *s, char *new_str, size_t *j, size_t *n)
+{
+	new_str[(*j)++] = '"';
+	*n = *j - 1;
+	while (s[*n])
+	{
+		new_str[*j] = s[*n];
+		(*j)++;
+		(*n)++;
+	}
+	new_str[(*j)++] = '"';
+	new_str[(*j)++] = '\0';
+}
+
 char	*ft_strdup_exp(const char *s)
 {
-	char	*i;
+	char	*new_str;
 	size_t	j;
 	size_t	z;
 	size_t	eq;
@@ -80,26 +98,13 @@ char	*ft_strdup_exp(const char *s)
 	n = 0;
 	eq = eq_len((char *)s);
 	while (s[z])
-	{
 		z++;
-	}
-	i = (char *)malloc(sizeof(char) * z + 3);
-	if (!i)
-		return (NULL);
+	new_str = (char *)safe_malloc(sizeof(char) * z + 3);
 	while (s[j] && j <= eq)
 	{
-		i[j] = s[j];
+		new_str[j] = s[j];
 		j++;
 	}
-	i[j++] = '"';
-	n = j - 1;
-	while (s[n])
-	{
-		i[j] = s[n];
-		j++;
-		n++;
-	}
-	i[j++] = '"';
-	i[j++] = '\0';
-	return ((char *)i);
+	last_iteration(s, new_str, &j, &n);
+	return ((char *)new_str);
 }
